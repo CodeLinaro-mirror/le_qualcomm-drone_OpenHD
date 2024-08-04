d64cfad81cc46e2812f77f83d48d71c4d70291f8
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-04 22:03:15 +0200

    Update openhd_led.h

--- OpenHD/ohd_common/inc/openhd_led.h
+++ OpenHD/ohd_common/inc/openhd_led.h
@@ -1,72 +1,34 @@
-// Created by consti10 on 01.02.24.
-// Refactored and improved Rapael 04.08.24
-//
+#ifndef OPENHD_LED_H
+#define OPENHD_LED_H
 
-#ifndef OPENHD_OPENHD_LED_H
-#define OPENHD_OPENHD_LED_H
-
-#include <atomic>
-#include <thread>
-#include <vector>
-#include <string>
 #include <chrono>
 #include <functional>
 
 namespace openhd {
 
-/**
- * OpenHD uses 2 LEDs (primary and secondary) for displaying 'stuff' to the user.
- * Whether those LEDs exist or not depends on the HW - here we abstract that
- * away.
- */
-class LEDManager {
- public:
-  static LEDManager& instance();
-  static constexpr int STATUS_OFF = 0;
-  static constexpr int STATUS_ON = 1;
+    class LEDManager {
+    public:
+        static LEDManager& instance();
 
-  void set_secondary_led_status(int status);
-  void set_primary_led_status(int status);
-  void set_aux_led_status(int status);
-  void set_rgb_led_status(int status, int color);
-  
-  // OpenHD is running and healthy
-  void set_status_okay();
-  // OpenHD is starting
-  void set_status_loading();
-  // OpenHD encountered an error
-  void set_status_error();
+        void set_status_loading();
+        void set_status_error();
+        void set_status_okay();
+        void set_primary_led_status(int status);
+        void set_secondary_led_status(int status);
 
- private:
-  explicit LEDManager();
-  ~LEDManager();
-  void loop();
-  void set_led_status(bool on, const std::function<void(bool)>& toggle_led_fn);
+    private:
+        LEDManager();
+        ~LEDManager();
+        
+        void blink_led(bool primary, std::chrono::milliseconds on_duration, std::chrono::milliseconds off_duration, int blink_count);
+        
+        void set_led_status(bool on, const std::function<void(bool)>& toggle_led_fn);
+        void set_aux_led_status(int status);
+        void set_rgb_led_status(int status, int color);
 
-  bool m_has_error = false;
-};
+        bool m_has_error;
+    };
 
-}  // namespace openhd
-
-std::vector<std::string> listLedFoldersWithBrightness(const std::string& baseDir);
-void setLedBrightness(const std::vector<std::string>& ledFolders, const std::string& baseDir, const std::string& value);
-void turnOffAllLeds(const std::vector<std::string>& ledFolders, const std::string& baseDir);
-void turnOnAllLeds(const std::vector<std::string>& ledFolders, const std::string& baseDir);
-
-namespace openhd::rpi {
-    void toggle_secondary_led(bool on);
-    void toggle_primary_led(bool on);
-    void toggle_led_delayed(bool on, const std::chrono::milliseconds& delay);
 }
 
-namespace openhd::zero3w {
-    void toggle_secondary_led(bool on);
-    void toggle_primary_led(bool on);
-}
-
-namespace openhd::radxacm3 {
-    void toggle_secondary_led(bool on);
-    void toggle_primary_led(bool on);
-}
-
-#endif  // OPENHD_OPENHD_LED_H
+#endif // OPENHD_LED_H
