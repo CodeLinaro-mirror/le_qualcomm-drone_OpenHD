b82f603ed099417dc38ab097f7274899f8951b54
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-04 22:04:07 +0200

    Revert "Update openhd_led.cpp"
    This reverts commit b7762b4d8a9dfab8b982e26425d235718dc0dd9c.

--- OpenHD/ohd_common/src/openhd_led.cpp
+++ OpenHD/ohd_common/src/openhd_led.cpp
@@ -2,13 +2,105 @@
 
 #include <chrono>
 #include <thread>
-#include <functional>
+#include <utility>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 #include "openhd_platform.h"
+#include "openhd_spdlog.h"
+#include "openhd_util.h"
 #include "openhd_util_filesystem.h"
 
-namespace openhd {
+std::vector<std::string> listLedFoldersWithBrightness(const std::string& baseDir) {
+    std::vector<std::string> ledFolders;
+    auto entries = OHDFilesystemUtil::getAllEntriesFullPathInDirectory(baseDir);
 
+    for (const auto& entry : entries) {
+        std::string brightnessFile = entry + "/brightness";
+        if (OHDFilesystemUtil::exists(brightnessFile)) {
+            std::string folderName = entry.substr(baseDir.length());
+            if (folderName.front() == '/') folderName = folderName.substr(1);
+            ledFolders.push_back(folderName);
+        }
+    }
+
+    return ledFolders;
+}
+
+void setLedBrightness(const std::vector<std::string>& ledFolders, const std::string& baseDir, const std::string& value) {
+    for (const auto& folder : ledFolders) {
+        std::string brightnessFile = baseDir + folder + "/brightness";
+        OHDFilesystemUtil::write_file(brightnessFile, value);
+    }
+}
+
+void turnOffAllLeds(const std::vector<std::string>& ledFolders, const std::string& baseDir) {
+    setLedBrightness(ledFolders, baseDir, "0");
+}
+
+void turnOnAllLeds(const std::vector<std::string>& ledFolders, const std::string& baseDir) {
+    setLedBrightness(ledFolders, baseDir, "1");
+}
+
+namespace openhd::rpi {
+    void toggle_led(const std::string& filename, bool on) {
+        if (!OHDFilesystemUtil::exists(filename)) {
+            return;
+        }
+        OHDFilesystemUtil::write_file(filename, on ? "1" : "0");
+    }
+
+    void toggle_secondary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/PWR/brightness";
+        toggle_led(filename, on);
+    }
+
+    void toggle_primary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/ACT/brightness";
+        toggle_led(filename, on);
+    }
+
+    void toggle_led_delayed(bool on, const std::chrono::milliseconds& delay) {
+        toggle_secondary_led(on);
+        std::this_thread::sleep_for(delay);
+        toggle_secondary_led(!on);
+    }
+}
+
+namespace openhd::zero3w {
+    void toggle_led(const std::string& filename, bool on) {
+        OHDFilesystemUtil::write_file(filename, on ? "1" : "0");
+    }
+
+    void toggle_secondary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/mmc0::/brightness";
+        toggle_led(filename, on);
+    }
+
+    void toggle_primary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/board-led/brightness";
+        toggle_led(filename, on);
+    }
+}
+
+namespace openhd::radxacm3 {
+    void toggle_led(const std::string& filename, bool on) {
+        OHDFilesystemUtil::write_file(filename, on ? "1" : "0");
+    }
+
+    void toggle_secondary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/pwr-led-red/brightness";
+        toggle_led(filename, on);
+    }
+
+    void toggle_primary_led(bool on) {
+        static constexpr auto filename = "/sys/class/leds/pi-led-green/brightness";
+        toggle_led(filename, on);
+    }
+}
+
+namespace openhd {
     LEDManager& LEDManager::instance() {
         static LEDManager instance{};
         return instance;
@@ -30,7 +122,46 @@ namespace openhd {
     }
 
     void LEDManager::set_rgb_led_status(int status, int color) {
-        // Color codes would be handled here if needed.
+        // List of colors and their corresponding numbers:
+        // 0 - Red
+        // 1 - Green
+        // 2 - Blue
+        // 3 - Cyan
+        // 4 - Magenta
+        // 5 - Yellow
+        // 6 - White
+        // 7 - Black
+
+        std::string colorName;
+
+        if (color == 0) {
+            colorName = "Red";
+            r = 1; g = 0; b = 0;
+        } else if (color == 1) {
+            colorName = "Green";
+            r = 0; g = 1; b = 0;
+        } else if (color == 2) {
+            colorName = "Blue";
+            r = 0; g = 0; b = 1;
+        } else if (color == 3) {
+            colorName = "Cyan";
+            r = 0; g = 1; b = 1;
+        } else if (color == 4) {
+            colorName = "Magenta";
+            r = 1; g = 0; b = 1;
+        } else if (color == 5) {
+            colorName = "Yellow";
+            r = 1; g = 1; b = 0;
+        } else if (color == 6) {
+            colorName = "White";
+            r = 1; g = 1; b = 1;
+        } else if (color == 7) {
+            colorName = "Black";
+            r = 0; g = 0; b = 0;
+        } else {
+            colorName = "Unknown";
+        }
+
     }
 
     void LEDManager::set_secondary_led_status(int status) {
@@ -107,13 +238,18 @@ namespace openhd {
         m_has_error = true;
     }
 
-    LEDManager::LEDManager() : m_has_error(false) {
+    LEDManager::LEDManager() {
     }
 
     LEDManager::~LEDManager() {
     }
 
+    void LEDManager::loop() {
+    }
+
     void LEDManager::set_status_okay() {
-        // Define and use your base directory and folders as needed
+        std::string baseDir = "/sys/class/leds/";
+        auto folders = listLedFoldersWithBrightness(baseDir);
+        turnOnAllLeds(folders, baseDir);
     }
 }
