37fd714a7c0613c51edc64d34bfd62540078e6f0
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-12 00:27:06 +0100

    Update openhd_config.cpp

--- OpenHD/ohd_common/src/openhd_config.cpp
+++ OpenHD/ohd_common/src/openhd_config.cpp
@@ -39,6 +39,9 @@ void openhd::set_config_file(const std::string& config_file_path) {
 }
 
 static openhd::Config load_or_default() {
+m_console = openhd::log::create_or_get(config);
+assert(m_console);
+  m_console->debug("DEBUG !!!!!!!!!!!!");
   try {
     openhd::Config ret{};
     if (!OHDFilesystemUtil::exists(CONFIG_FILE_PATH)) {
